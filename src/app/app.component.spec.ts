c636c513822764ad85edf46654f4af11db2fe123
kulshekhar <kulshekhar@users.noreply.github.com>
2017-05-06 16:30:40 +0530

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { IonicModule } from "ionic-angular";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        IonicModule.forRoot(AppComponent)
       ],
       declarations: [
         AppComponent
